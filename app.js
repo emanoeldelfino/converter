d1163b05573b971b4897920abdf6cbffc45a25e9
emanoeldelfino <emanoeldelfno@gmail.com>
2021-11-17 19:54:24 -0300

    Update style, update temperature and base converter

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import { valueToBase } from "./baseConverter.js";
+import { convertTemp } from "./tempConverter.js";
 
 const bases = Array.from({ length: 35 }, (v, k) => k + 2);
+bases[0] = "2 (Binary)";
+bases[6] = "8 (Octal)";
+bases[8] = "10 (Decimal)";
+bases[14] = "16 (Hexadecimal)";
+
 const temps = ["Celsius", "Fahrenheit", "Kelvin"];
 const inputSelect = document.querySelector("#input-unit");
 const outputSelect = document.querySelector("#output-unit");
@@ -29,19 +35,22 @@ conversionUnit.addEventListener("change", (elem) => {
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
+
+  let input = inputNum.value;
+  let inputUnit = parseInt(inputSelect.value.split(" ")[0]);
+  let outputUnit = parseInt(outputSelect.value.split(" ")[0]);
+
+  let output = "";
+
   if (conversionUnit.value === "base") {
-    let input = inputNum.value;
-    let inputUnit = inputSelect.value;
-    let outputUnit = outputSelect.value;
-
-    let output = "";
-
+    output = valueToBase(input, inputUnit, outputUnit);
     try {
       output = valueToBase(input, inputUnit, outputUnit);
     } catch (err) {
       alert(err);
     }
-
-    document.querySelector("#output-num").innerText = output;
+  } else if (conversionUnit.value === "temp") {
+    output = convertTemp(input, inputUnit, outputUnit);
   }
+  document.querySelector("#output-num").innerText = output;
 });
---
+++ tempConverter.js
@@ -0,0 +1,30 @@
+export function celsiusToTemp(celsius, outTemp) {
+  outTemp = outTemp.toUpperCase();
+
+  if (outTemp[0] === "C") {
+    return celsius;
+  } else if (outTemp[0] === "F") {
+    return (celsius * 9) / 5 + 32;
+  } else if (outTemp[0] === "K") {
+    return celsius + 273.15;
+  }
+}
+
+export function tempToCelsius(temp, inTemp) {
+  inTemp = inTemp.toUpperCase();
+
+  if (inTemp[0] === "C") {
+    return temp;
+  } else if (inTemp[0] === "F") {
+    return ((temp - 32) * 5) / 9;
+  } else if (inTemp[0] === "K") {
+    return temp - 273.15;
+  }
+}
+
+export function convertTemp(temp, inTemp, outTemp) {
+  const tempCelsius = tempToCelsius(temp, inTemp);
+  const tempConverted = celsiusToTemp(tempCelsius, outTemp);
+
+  return tempConverted;
+}
